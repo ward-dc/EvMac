cc3a76287628560bdc709d3c3cba7980d66e9b64
PoweredByWard <warddecoster2003@gmail.com>
2021-01-28 20:53:02 +0100

    can not close on mac fix

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, screen, clipboard, dialog} = require("electron");
+const { app, BrowserWindow, screen, clipboard, dialog } = require("electron");
 const shortcut = require("electron-localshortcut");
 const path = require("path");
 const prompt = require("electron-prompt");
@@ -10,12 +10,12 @@ function Init() {
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
-    removeMenu:true,
+    removeMenu: true,
   });
   mainWindow.on("close", () => {
     mainWindow = null;
     app.quit();
- });
+  });
   mainWindow.setFullScreen(true);
   mainWindow.loadURL("https://ev.io/");
   mainWindow.webContents.on("did-finish-load", (event) => {
@@ -84,10 +84,10 @@ function createNewWindow(url, mainWindow) {
     height: 780,
     show: false,
     parent: mainWindow,
-    removeMenu:true,
+    removeMenu: true,
     webPreferences: {
       nodeIntegration: true,
-    }
+    },
   });
   win.setSimpleFullScreen(false);
   win.loadURL(url);
@@ -108,81 +108,75 @@ function createNewWindow(url, mainWindow) {
   win.webContents.on("did-finish-load", (event) => {
     if (win.webContents.getURL() == "https://ev.io/") {
       win.close();
-      mainWindow.webContents.reload();
-      mainWindow.loadURL("https://ev.io/")
+      setTimeout(() => {
+        mainWindow.loadURL("https://ev.io/");
+      }, 500);
     }
   });
 }
 
-
-
-
-
-
-
-const { autoUpdater } = require('electron-updater');
-console.log('autoUpdater entered');
-autoUpdater.logger = require('electron-log');
-autoUpdater.logger.transports.file.level = 'info';
-autoUpdater.checkForUpdates()
-autoUpdater.on('checking-for-update', () => {
-    console.log('Checking for updates...');
+const { autoUpdater } = require("electron-updater");
+console.log("autoUpdater entered");
+autoUpdater.logger = require("electron-log");
+autoUpdater.logger.transports.file.level = "info";
+autoUpdater.checkForUpdates();
+autoUpdater.on("checking-for-update", () => {
+  console.log("Checking for updates...");
 });
-autoUpdater.on('update-available', (info) => {
-    const dialogOpts = {
-        type: 'info',
-        buttons: ['Alright!'],
-        title: 'EvClient Update',
-        message: 'New Version of EvClient has been released',
-        detail: 'It will be downloaded in the background and notify you when the download is finished.'
-       }
-  
-       dialog.showMessageBox(dialogOpts).then((returnValue) => {
-         if (returnValue.response === 0)
-         console.log('Version message seen')
-       })
+autoUpdater.on("update-available", (info) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Alright!"],
+    title: "EvClient Update",
+    message: "New Version of EvClient has been released",
+    detail:
+      "It will be downloaded in the background and notify you when the download is finished.",
+  };
+
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) console.log("Version message seen");
+  });
 });
-autoUpdater.on('update-not-available', () => {
-    console.log('Version is up-to-date');
+autoUpdater.on("update-not-available", () => {
+  console.log("Version is up-to-date");
 });
-autoUpdater.on('download-progress', (progressObj) => {
-    console.log(`Download Speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.transferred} + '/ ${progressObj.total}`);
+autoUpdater.on("download-progress", (progressObj) => {
+  console.log(
+    `Download Speed: ${progressObj.bytesPerSecond} - Downloaded ${progressObj.transferred} + '/ ${progressObj.total}`
+  );
 });
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-      type: 'info',
-      buttons: ['Restart', 'Later'],
-      title: 'Application Update',
-      message: process.platform === 'win32' ? releaseNotes : releaseName,
-      detail: 'A new version has been downloaded. Restart the application to apply the updates.'
-     }
+autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+  const dialogOpts = {
+    type: "info",
+    buttons: ["Restart", "Later"],
+    title: "Application Update",
+    message: process.platform === "win32" ? releaseNotes : releaseName,
+    detail:
+      "A new version has been downloaded. Restart the application to apply the updates.",
+  };
 
-     dialog.showMessageBox(dialogOpts).then((returnValue) => {
-       if (returnValue.response === 0) autoUpdater.quitAndInstall()
-     })
-   })
-autoUpdater.on('error', (error) => {
-    console.log(error)
-})
-
-
-
+  dialog.showMessageBox(dialogOpts).then((returnValue) => {
+    if (returnValue.response === 0) autoUpdater.quitAndInstall();
+  });
+});
+autoUpdater.on("error", (error) => {
+  console.log(error);
+});
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   Init();
-  
+
   app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) Init();
   });
 });
-app.on('browser-window-created',function(e,window) {
+app.on("browser-window-created", function (e, window) {
   window.setMenu(null);
-  window.setFullScreen(false);
 });
 
 // Quit when all windows are closed, except on macOS. There, it's common
